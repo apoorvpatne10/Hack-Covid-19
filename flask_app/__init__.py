def7837cd90fe99e41841a4e55c41fa923c334a5
apoorvpatne10 <apoorv.patne10@gmail.com>
2020-04-12 20:01:27 +0530

    Added secret key and allowed file types

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'flask_app/uploads'
+app.config['SECRET_KEY'] = '1b706efb9c107f1427893320fca12686'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['ALLOWED_FILE_TYPES'] = ['JSON', 'TXT']
 db = SQLAlchemy(app)
 
 
